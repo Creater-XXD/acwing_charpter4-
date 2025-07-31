5bb57c1a9199690456a1e795b7b35ae3e587c2a6
新冬 夏 <2504251661@qq.com>
2025-07-31 14:50:49 +0800

    使用qsort替换手写快排

--- 试除法找约数/main.c
+++ 试除法找约数/main.c
@@ -1,69 +1,50 @@
 #define _CRT_SECURE_NO_WARNINGS
-#include<stdio.h>
-#include<string.h>
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
 
 #define N 2000
 
 int res[N];
 
-void quick_sort(int q[], int l, int r)
-{
-	if (l >= r) return;
-
-	int i = l - 1, j = r + 1;
-	int x = q[l + r >> 1];
-
-	while (i < j)
-	{
-		do i++; while (q[i] < x);
-		do j--; while (q[j] > x);
-
-		if (i < j)
-		{
-			int tmp = q[i];
-			q[i] = q[j];
-			q[j] = tmp;
-		}
-	}
-
-	quick_sort(q, l, j);
-	quick_sort(q, j + 1, r);
+int cmp_int(const void* a, const void* b) {
+    return (*(int*)a) - (*(int*)b);
 }
 
 void get_divisor(int n)
 {
-	memset(res, -1, sizeof res);
-	int cnt = 0;
-	
-	for (int i = 1; i <= n / i; i++)
-	{
-		if (n % i == 0)
-		{
-			res[cnt++] = i;
-			if (i != n / i) res[cnt++] = n / i;
-		}
-	}
+    memset(res, -1, sizeof res);
+    int cnt = 0;
+    
+    for (int i = 1; i <= n / i; i++)
+    {
+        if (n % i == 0)
+        {
+            res[cnt++] = i;
+            if (i != n / i) res[cnt++] = n / i;
+        }
+    }
 
-	quick_sort(res, 0, cnt - 1);
+    qsort(res, cnt, sizeof(int), cmp_int);
 
-	for (int i = 0; i < cnt; i++)
-	{
-		printf("%d ", res[i]);
-	}
-	puts("");
+    for (int i = 0; i < cnt; i++)
+    {
+        printf("%d ", res[i]);
+    }
+    puts("");
 }
 
 int main(void)
 {
-	int n;
-	scanf("%d", &n);
+    int n;
+    scanf("%d", &n);
 
-	while (n--)
-	{
-		int x;
-		scanf("%d", &x);
+    while (n--)
+    {
+        int x;
+        scanf("%d", &x);
 
-		get_divisor(x);
-	}
-	return 0;
+        get_divisor(x);
+    }
+    return 0;
 }
